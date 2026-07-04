01fb0f3c763aaa9781e64077e9219310e3b7ed6e
Matthew <matthewmatt123455@gmail.com>
2026-07-04 01:08:32 +0100

    added more placeholders, made container dynamic so it doesnt intersec with the wall, added mobile CSS (now works on mobile)

--- Pages/Programming/scripts.js
+++ Pages/Programming/scripts.js
@@ -7,18 +7,36 @@ function expand(img, contentToDisplay, description, gitLink)
     let expandedHeader = document.querySelector(".desc");
     let expandedGitLink = document.querySelector(".gitLink");
 
-    let xPos = (window.scrollX + img.getBoundingClientRect().left) - 8;
-
-    expandedGitLink.href = gitLink;
-    expandedHeader.innerHTML = description;
-
-    expandedVideo.parentElement.style.marginLeft = String(xPos).concat("px");
-
     expandedVideo.src = contentToDisplay;
     expandedVideo.parentElement.style.display = "block";
     expandedVideo.load();
     expandedVideo.play();
+
+    if (window.matchMedia("(max-width: 800px)").matches) // Mobile View
+    {
+        img.parentElement.appendChild(expandedVideo.parentElement);
+
+        expandedVideo.width = "320"
+        expandedVideo.height = "180"
+
+        expandedGitLink.href = gitLink;
+        expandedHeader.innerHTML = description;
+    }
+    else // Desktop View
+    {
+        expandedGitLink.href = gitLink;
+        expandedHeader.innerHTML = description;
+
+        if ((window.scrollX + img.getBoundingClientRect().left) - 8 >= window.innerWidth/2){
+            expandedVideo.parentElement.style.marginLeft = String((window.scrollX + img.getBoundingClientRect().right) - expandedVideo.parentElement.offsetWidth).concat("px");
+        }
+        else {
+            expandedVideo.parentElement.style.marginLeft = String((window.scrollX + img.getBoundingClientRect().left) - 8).concat("px");
+        }
+    }
 }
+    
+   
 function collapse(btn)
 {
     let expandedVideo = document.querySelector(".expandedMedia");
